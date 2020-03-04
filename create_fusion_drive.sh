b3e71fec00a341014a295c811bb160ff319a6410
Justin Lloyd <justin@Justins-Mac.local>
2020-03-03 22:16:02 -0800

    Update readme. Change hard-coded RAM disk size to variable definition.

--- create_fusion_drive.sh
+++ create_fusion_drive.sh
@@ -7,8 +7,9 @@ fi
 
 BACKING_STORE_DISK=$1
 
+RAM_DISK_CAPACITY=8
 diskutil eraseDisk HFS+ BackingStore /dev/${BACKING_STORE_DISK}
-RAM_DISK_ID=$(hdiutil attach -nomount ram://$((16*1024*1024)))
+RAM_DISK_ID=$(hdiutil attach -nomount ram://$((2*${RAM_DISK_CAPACITY}*1024*1024)))
 CACHE_DISK=$(diskutil eraseDisk HFS+ "RAM Disk" ${RAM_DISK_ID} | tail -n 1 | sed 's/Finished erase on //')
 TIERED_DISK=$(diskutil apfs createContainer -main ${CACHE_DISK}s2 -secondary ${BACKING_STORE_DISK}s2 | grep 'Disk from APFS operation' | sed 's/Disk from APFS operation\: //')
 diskutil apfs addVolume ${TIERED_DISK} APFS WorkDrive
